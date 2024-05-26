084f12ce5c1901c3070cf788e172b8560a428dc6
Bắc Nguyễn <hoaibac1103@gmail.com>
2024-05-26 14:56:48 +0700

    draft

---
+++ WebBanHang/src/main/java/shop/controller/AuthController.java
@@ -0,0 +1,13 @@
+package shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
