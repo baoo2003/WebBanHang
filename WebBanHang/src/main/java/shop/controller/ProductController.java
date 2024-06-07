5676263bb34e9308ca0d74cad3ccdae07dbfc515
Bắc Nguyễn <hoaibac1103@gmail.com>
2024-06-07 16:28:11 +0700

    view all products

--- WebBanHang/src/main/java/shop/controller/ProductController.java
+++ WebBanHang/src/main/java/shop/controller/ProductController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import shop.dto.request.FilterProductDto;
 import shop.entity.Category;
 import shop.entity.Product;
 import shop.service.ProductService;
@@ -45,5 +48,43 @@ public class ProductController {
 			model.addAttribute("keyWord", keyWord);
 		}		
 		return "Product";
+	}
+	
+	@RequestMapping("/manage-product")
+	public String manageProduct(
+		ModelMap model,
+		@RequestParam(value = "page", defaultValue = "1") Long page,
+		@ModelAttribute("filter") FilterProductDto filter
+	) {
+		final int limit = 8;
+		
+		Long numOfProducts = productService.countRecord(filter.getCategoryId(), filter.getPriceGroupId(), filter.getKeyword());
+		int numOfPages = (int) Math.ceil((double) numOfProducts / limit);
+		model.addAttribute("numOfPages", numOfPages);
+		
+		model.addAttribute("filter", new FilterProductDto());
+		
+		List<Product> products = productService.getProducts(page, limit, filter.getCategoryId(), filter.getPriceGroupId(), filter.getKeyword());
+		model.addAttribute("products", products);
+		return "admin/product/index";
+	}
+	
+	@RequestMapping(value = "/manage-product", method = RequestMethod.POST)
+	public String mangeProduct(
+		ModelMap model,
+		@RequestParam(value = "page", defaultValue = "1") Long page,
+		@ModelAttribute("filter") FilterProductDto filter
+	) {
+		final int limit = 8;
+		
+		List<Product> products = productService.getProducts(page, limit, filter.getCategoryId(), filter.getPriceGroupId(), filter.getKeyword());
+		model.addAttribute("products", products);
+		
+		return "admin/product/index";
+	}
+	
+	@ModelAttribute("categories")
+	public List<Category> getCategories() {
+		return productService.getCategories();
 	}
 }
