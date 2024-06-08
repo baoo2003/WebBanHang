7fbbc0ec8ef41bc948a9ed69f23c13f9ee5af466
quangtrungit1106 <quangtrungit1106@gmail.com>
2024-06-08 17:22:10 +0700

    fix month earning

--- WebBanHang/src/main/webapp/resources/js/dashboard.js
+++ WebBanHang/src/main/webapp/resources/js/dashboard.js
@@ -179,7 +179,7 @@ $(function () {
       {
         name: "Earnings",
         color: "#49BEFF",
-        data: [25, 66, 20, 40, 12, 58, 20],
+        data: salesData,
       },
     ],
     stroke: {
