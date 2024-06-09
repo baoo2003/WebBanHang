55b4d9b45975ed3d7728a05c9f503f46b447f763
DamTuanPhat <damtuanphat@gmail.com>
2024-06-09 10:57:47 +0700

    update: order serivce

---
+++ WebBanHang/src/main/java/shop/controller/OrderController.java
@@ -0,0 +1,45 @@
+package shop.controller;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import shop.dto.request.ProfileDto;
+import shop.entity.Customer;
+import shop.entity.Order;
+import shop.service.OrderService;
+
+@Controller
+public class OrderController {
+	@Autowired
+	private OrderService orderService;
+
+	@RequestMapping("/customer-order")
+	public String index(ModelMap model, HttpSession session) {
+		Integer customerIdInt = (Integer) session.getAttribute("userId");
+
+		if (customerIdInt == null) {
+			return "redirect:/login.htm";
+		}
+
+		List<Object[]> orders = orderService.getOrders(Optional.empty(), Optional.empty(), Optional.of(customerIdInt));
+
+		model.addAttribute("orders", orders);
+		return "Customer-order";
+	}
+}
--- WebBanHang/src/main/java/shop/service/OrderService.java
+++ WebBanHang/src/main/java/shop/service/OrderService.java
@@ -1,6 +1,7 @@
 package shop.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Map;
 import java.util.Date;
 import java.util.HashMap;
@@ -14,8 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import shop.dto.request.OrderDto;
+import shop.dto.request.UpdateProductDto;
+import shop.entity.Brand;
 import shop.entity.Cart;
 import shop.entity.CartId;
+import shop.entity.Category;
 import shop.entity.Customer;
 import shop.entity.Order;
 import shop.entity.OrderDetail;
@@ -30,11 +34,10 @@ public class OrderService {
 
 	@Autowired
 	private CartService cartService;
-	
+
 	@Autowired
 	private CustomerService customerService;
 
-	
 	@Transactional
 	public void createOrder(OrderDto orderDto, Integer customerId) {
 		Session session = sessionFactory.openSession();
@@ -43,7 +46,7 @@ public class OrderService {
 			List<Object[]> cartDetails = cartService.getCartAndProductDetailsByCustomer(customerId);
 			Customer customer = customerService.getCustomerById(customerId);
 			Order order = new Order();
-			
+
 			Date now = new Date();
 
 			order.setFullname(orderDto.getFirstName() + " " + orderDto.getLastName());
@@ -53,38 +56,74 @@ public class OrderService {
 			order.setDeliveryNote(orderDto.getNote());
 			order.setCustomer(customer);
 			order.setOrderTime(now);
-			now.setDate(7);
-			order.setDeliveryTime(now);
 			order.setDeliveryNote(orderDto.getNote());
 			order.setStatus("PLACED");
-			
+
 			session.save(order);
-			
 
 			for (Object[] detail : cartDetails) {
 				Integer productId = (Integer) detail[1];
 				Integer quantity = (Integer) detail[2];
 				Float price = (Float) detail[4];
-				
+
 				OrderDetailId orderDetailId = new OrderDetailId(order.getId(), productId);
 				OrderDetail orderDetail = new OrderDetail();
-				
+
 				Product product = new Product();
-				
+
 				product.setId(productId);
-				
+
 				orderDetail.setId(orderDetailId);
 				orderDetail.setOrder(order);
 				orderDetail.setPrice(price);
 				orderDetail.setProduct(product);
 				orderDetail.setQuantity(quantity);
 				session.save(orderDetail);
-	        }
-			
+			}
+
 			transaction.commit();
 
 			cartService.deleteCartByCustomerId(customerId);
 		} catch (Exception e) {
+			transaction.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
+	public List<Object[]> getOrders(Optional<Integer> page, Optional<Integer> limit, Optional<Integer> customerId) {
+		Session session = sessionFactory.openSession();
+		String hql = "SELECT o.id, o.customer, o.fullname, o.phoneNumber, o.address, o.deliveryTime, o.orderTime, o.status, o.cancelReason, o.deliveryNote FROM Order o";
+		if (customerId.isPresent()) {
+			hql = "SELECT o.id, o.customer, o.fullname, o.phoneNumber, o.address, o.deliveryTime, o.orderTime, o.status, o.cancelReason, o.deliveryNote FROM Order o WHERE o.customer.id = :customerId";
+		}
+		Query query = session.createQuery(hql);
+		if (customerId.isPresent()) {
+			query.setParameter("customerId", customerId.get());
+		}
+		if (page.isPresent() && limit.isPresent()) {
+
+			query.setFirstResult((int) ((page.get() - 1) * limit.get()));
+			query.setMaxResults(limit.get());
+		}
+		List<Object[]> results = query.list();
+
+		return results;
+	}
+
+	@Transactional
+	public void updateOrder(int OrderId, OrderDto orderDto) {
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		try {
+			Order order = (Order) session.get(Order.class, OrderId);
+
+			session.update(order);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
 			transaction.rollback();
 			throw e;
 		} finally {
